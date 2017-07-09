1a5abbde528e95a24bf3216b18edfb0a64f548fe
Allie Grampa <agrampa@yahoo.com>
2017-07-09 13:40:08 -0700

    bundle.js added

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
 const ce = React.createElement
 
 const MyTitle = function(props) {
@@ -17,7 +20,12 @@ const MyFirstComponent = function() {
   )
 }
 
-ReactDOM.render(
+render(
   ce(MyFirstComponent),
   document.getElementById('app')
 )
+
+// ReactDOM.render(
+//   ce(MyFirstComponent),
+//   document.getElementById('app')
+// )
