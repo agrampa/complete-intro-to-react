3711033fa428107486f8948f302980e40d0f0af2
Allie Grampa <agrampa@yahoo.com>
2017-07-09 16:19:30 -0700

    refactored for JSX

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement
-
 const MyTitle = function(props) {
-  // return (
-  //   ce('div', null,
-  //     ce('h1', { style: { color: props.color } }, props.title)
-  //   )
-  // )
+
   const style = { color: props.color };
 
-  return (
+  /////////
+  // JSX //
+  ////////
+  return (  // paren is needed so it keeps reading to the next line
     <div>
       <h1 style={ style }>{ props.title }</h1>
     <div>
@@ -21,20 +18,17 @@ const MyTitle = function(props) {
 }
 
 const MyFirstComponent = function() {
-  return ce('div', null,
-    ce(MyTitle, {title: 'Game of Thrones', color: 'YellowGreen'}),
-    ce(MyTitle, {title: 'Seinfeld', color: 'GreenYellow'}),
-    ce(MyTitle, {title: 'Stranger Things', color: 'LimeGreen'}),
-    ce(MyTitle, {title: 'Rick & Morty', color: 'peru'})
-  )
+  // paren is not needed here because there is other content after the "return"
+  return <div id="my-first-component">
+    <MyTitle title = "Game of Thrones" color="YellowGreen" />
+    <MyTitle title = "Seinfeld" color="GreenYellow" />
+    <MyTitle title = "Stranger Things" color="LimeGreen" />
+    <MyTitle title = "Rick & Morty" color="peru" />
+  </div>
+
+  // cannot put in another top-level element at the same level as the div, it can only return one element at the top level at a time
+  // will be possible in react 16
+  // for now, wrap all attributes needed inside of one top-level div 
 }
 
-render(
-  ce(MyFirstComponent),
-  document.getElementById('app')
-)
-
-// ReactDOM.render(
-//   ce(MyFirstComponent),
-//   document.getElementById('app')
-// )
+render(<MyFirstComponent />, document.getElementById('app'));
