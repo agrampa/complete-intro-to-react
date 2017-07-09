ad5882f2ff1a2742487e29cf5f5f3b16c821da33
Allie Grampa <agrampa@yahoo.com>
2017-07-09 16:42:08 -0700

    added devServer

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     filename: 'bundle.js'
   },
   devServer: {
-    publicPath: '/public'
+    publicPath: '/public/'
   },
   resolve: {
     extensions: ['.js', '.jsx', '.json']
