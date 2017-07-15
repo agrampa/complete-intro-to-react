69242dbeacfc787624bcf5c49430b114c3c4acd2
Allie Grampa <agrampa@yahoo.com>
2017-07-15 09:55:49 -0700

    added ShowCard.js file

--- js/Search.js
+++ js/Search.js
@@ -10,17 +10,12 @@ const Search = () => (
     // {preload.shows.map( show => <h3>{show.title}</h3> )} // no parens around show, only one param
 
     {preload.shows.map(show => (
-      <div className="show-card">
-        <img alt={`${show.title} Show Poster`} src={`/public/img/posters/${show.poster}`} />
-        <div>
-          <h3>{show.title}</h3>
-          <h4>({show.year})</h4>
-          <p>{show.description}</p>
-        </div>
-      </div>
+      
     ))}
-
   </div>
 );
 
 export default Search;
+
+
+// use alt+command+period to add closing tag!
---
+++ js/ShowCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ShowCard = props => (
+  <div className="show-card">
+    <img alt={`${show.title} Show Poster`} src={`/public/img/posters/${show.poster}`} />
+    <div>
+      <h3>{show.title}</h3>
+      <h4>({show.year})</h4>
+      <p>{show.description}</p>
+    </div>
+  </div>
+);
+
+export default ShowCard;
