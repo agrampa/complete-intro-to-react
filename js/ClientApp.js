63f8d071a7bbbb0435181f190effe7238f2904af
Allie Grampa <agrampa@yahoo.com>
2017-07-15 09:13:17 -0700

    resolved merge conflicts, added landing.js

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
+import Landing from './Landing.js';
 
 const App = () => ( // use paren with arrow functions when the first line is a return
     // need to use "className" bc "class" is a reserved word
-  <div className='landing'>
-    <h1>svideo</h1>
-    <input type='text' placeholder='Search' />
-    <a>or Browse All</a>
-  </div>
+  <HashRouter>
+    <div className="app">
+      // will render Landing if the exact path is matched, otherwise render nothing
+      <Route exact path="/" component={Landing} />
+    </div>
+  </HashRouter>
+);
 
 render(<App />, document.getElementById('app'));
+
+// HashRouter is a higher-order component -> doesn't render anything out by itself, it will route then you tell it what markup to render
+// introduces routing behavior, but doesn't render anything out of it
