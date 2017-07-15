7356603fb11e9a6c1495d0df37602d993da1b438
Allie Grampa <agrampa@yahoo.com>
2017-07-15 09:51:06 -0700

    added logic to load JSON data for each show

--- js/Search.js
+++ js/Search.js
@@ -2,12 +2,24 @@ import React from 'react';
 import preload from '../data.json';
 
 const Search = () => (
-  <div className='search'>
-    // transform array of show data into an array of show components == use .map
-    {preload.shows.map( (show) => <h3>{show.title}</h3> )}
-
+  <div className="search">
     // // preformat and print out whatever is in preload
     // <pre><code>{JSON.stringify(preload, null, 4)}</code></pre> // debugging trick to see JSON printout
+
+    // transform array of show data into an array of show components == use .map
+    // {preload.shows.map( show => <h3>{show.title}</h3> )} // no parens around show, only one param
+
+    {preload.shows.map(show => (
+      <div className="show-card">
+        <img alt={`${show.title} Show Poster`} src={`/public/img/posters/${show.poster}`} />
+        <div>
+          <h3>{show.title}</h3>
+          <h4>({show.year})</h4>
+          <p>{show.description}</p>
+        </div>
+      </div>
+    ))}
+
   </div>
 );
 
