41a7296fb9947163f66e8746e8b6a99d403b250e
Allie Grampa <agrampa@yahoo.com>
2017-07-18 09:50:08 -0700

    refactored app.js and clientapp.js for HMR functionality

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,29 +1,51 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Landing from './Landing.js';
-import Search from './Search.js';
+import App from './App'
+
+
+// this is the place for browser code, not anything used by nodes
+
+
+
+const renderApp = () => {
+  render(<App />, document.getElementById('app'));
+};
+
+renderApp();
+
+
+// if hot module replacement is enabled, for this build, module.hot will exist, so you are in development
+// any time App changes, call the function again to rerender the whole app
+// plugins make it possible for the modules to replace themselves
+// React knows how to call the renderApp function multiple times
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    renderApp();
+  });
+}
+
+
 
 // Commenting out html/jsx in js files: anything in between {/* */} will be an inline comment
 
-const FourOhFour = () => <h1>404</h1>
-
-const App = () => ( // use paren with arrow functions when the first line is a return
-    // need to use "className" bc "class" is a reserved word
-  <BrowserRouter>
-    <div className="app">
-      <Switch>
-        // will render Landing if the exact path is matched, otherwise render nothing
-        <Route exact path="/" component={Landing} />
-        <Route path="/search" component={Search} />
-        // renders when nothing else is matched
-        <Route component={FourOhFour} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
-
-render(<App />, document.getElementById('app'));
+// // moved to App.js for hot module replacement
+// const FourOhFour = () => <h1>404</h1>
+//
+// const App = () => ( // use paren with arrow functions when the first line is a return
+//     // need to use "className" bc "class" is a reserved word
+//   <BrowserRouter>
+//     <div className="app">
+//       <Switch>
+//         // will render Landing if the exact path is matched, otherwise render nothing
+//         <Route exact path="/" component={Landing} />
+//         <Route path="/search" component={Search} />
+//         // renders when nothing else is matched
+//         <Route component={FourOhFour} />
+//       </Switch>
+//     </div>
+//   </BrowserRouter>
+// );
+//
 
 // BrowserRouter fixes routes in search bar instead of having # in url
 // Better for SEO
